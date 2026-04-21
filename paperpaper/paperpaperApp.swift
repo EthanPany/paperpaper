ad7688eb2db96217991015f40d969d3d709322eb
Ethan Pan <pyy122759996@gmail.com>
2026-04-21 14:32:06 -0400

    Initial Commit

---
+++ paperpaper/paperpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  paperpaperApp.swift
+//  paperpaper
+//
+//  Created by Ethan Pan on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct paperpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
